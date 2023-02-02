6b8175d6fdff6e70ee2612d00db3d95c693ed638
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-02-02 15:04:28 +0530

    Signup form created

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,18 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function Login() {
+
+    const onSubmit = ev => {
+        ev.preventDefault()
+
+    }
     return(
         <div className="login">
         <div className="form">
           <form onSubmit={onSubmit}>
             <h1 className="title">Login into your account</h1>
 
-            {message &&
-              <div className="alert">
-                <p>{message}</p>
-              </div>
-            }
-
-            <input ref={emailRef} type="email" placeholder="Email"/>
-            <input ref={passwordRef} type="password" placeholder="Password"/>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
             <button className="btn btn-block">Login</button>
             <p className="message">Not registered? <Link to="/signup">Create an account</Link></p>
           </form>
