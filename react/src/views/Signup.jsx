4ff9b6c77999b6af151cd5a0476b84fe047232d9
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-02-02 15:25:39 +0530

    changes to onSubmit

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,22 +1,39 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import {createRef, useState} from "react";
 export default function Signup() {
 
-    const onSubmit = ev => {
+    const nameRef = createRef();
+    const emailRef = createRef();
+    const contactRef = createRef();
+    const addressRef = createRef();
+    const passwordRef = createRef();
+    const passwordConfirmationRef = createRef();
+
+    const onSubmit = (ev) => {
         ev.preventDefault()
 
+        const payload = {
+
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            contact: contactRef.current.value,
+            address: addressRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload);
     }
     return (
         <div className="login-signup">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for Free</h1>
-                    <input type="text" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="text" placeholder="Contact Number" />
-                    <input type="address" placeholder="Address" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Repeat Password" />
+                    <input  ref={nameRef} type="text" placeholder="Name" />
+                    <input  ref={emailRef} type="email" placeholder="Email" />
+                    <input  ref={contactRef} type="text" placeholder="Contact Number" />
+                    <input  ref={addressRef} type="address" placeholder="Address" />
+                    <input  ref={passwordRef} type="password" placeholder="Password" />
+                    <input  ref={passwordConfirmationRef} type="password" placeholder="Repeat Password" />
                     <button className="btn btn-block">Signup</button>
                     <p className="message">Already registered? <Link to="/login">Sign In</Link></p>
                 </form>
