6b23b66f58bff13efd0768c4609420cee90725d2
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-02-02 19:03:48 +0530

    User Crud implemented

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,25 @@
 export default function Users() {
-    return(
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    //Get All Users
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({ data }) => {
+                setLoading(false)
+                setUsers(data.data)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
+    return (
         <>
             Users
         </>
